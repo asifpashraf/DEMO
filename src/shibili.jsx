0e6d199873159d4b1245710bcd46c1f01d867226
codebyshibili <shibili7557@gmail.com>
2024-08-28 15:49:15 +0530

    name added

---
+++ src/shibili.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Shibili() {
+  return (
+    <div className=''> 
+        <h1 className=''>MOHAMMED SHIBILI PK</h1>
+    </div>
+  )
+}
+
+export default Shibili
